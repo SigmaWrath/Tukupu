25cbe8b6364dfc499002c09a356fc76949157e34
hulinjiang <hulinjiang@58.com>
2024-12-02 20:03:09 +0800

    feat: 内部坏链检测

--- quartz/components/scripts/popover.inline.ts
+++ quartz/components/scripts/popover.inline.ts
@@ -8,7 +8,7 @@ async function mouseEnterHandler(
   { clientX, clientY }: { clientX: number; clientY: number },
 ) {
   const link = this
-  if (link.dataset.noPopover === "true") {
+  if (link.dataset.noPopover === "true" || link.classList.contains('broken-link')) {
     return
   }
 
@@ -103,7 +103,9 @@ async function mouseEnterHandler(
 }
 
 document.addEventListener("nav", () => {
-  const links = [...document.getElementsByClassName("internal")] as HTMLAnchorElement[]
+  const links = [...document.getElementsByClassName("internal")].filter(
+    link => !link.classList.contains('broken-link')
+  ) as HTMLAnchorElement[]
   for (const link of links) {
     link.addEventListener("mouseenter", mouseEnterHandler)
     window.addCleanup(() => link.removeEventListener("mouseenter", mouseEnterHandler))
